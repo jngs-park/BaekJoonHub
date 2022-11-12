e132045b536c1982c742fa1a57132374430bc096
Jongsoo Park <dear.jayp@gmail.com>
2022-11-12 18:01:24 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 228 ms, Memory: 18348 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int k, q, l, b, n, p;
+        k = sc.nextInt();
+        q = sc.nextInt();
+        l = sc.nextInt();
+        b = sc.nextInt();
+        n = sc.nextInt();
+        p = sc.nextInt();
+        
+         System.out.print(1-k + " ");
+         System.out.print(1-q + " ");
+         System.out.print(2-l + " ");
+         System.out.print(2-b + " ");
+         System.out.print(2-n + " ");
+         System.out.print(8-p + " ");
+    }
+}
