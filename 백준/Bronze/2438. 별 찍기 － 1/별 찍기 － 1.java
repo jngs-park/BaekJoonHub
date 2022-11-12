c6242e005e1b79b898a88255c5017af1c27b7a68
Jongsoo Park <dear.jayp@gmail.com>
2022-11-13 00:24:11 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 304 ms, Memory: 17952 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		for(int i=1; i<=a; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");	
+			}
+			System.out.print("\n");	
+	}
+		
+}
+}
