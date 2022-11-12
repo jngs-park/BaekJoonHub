e09138b5a59acef1363f29a8001e6e04c0c08fa7
Jongsoo Park <dear.jayp@gmail.com>
2022-11-13 02:45:27 +0900

    [Bronze III] Title: 최댓값, Time: 204 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int[] arr = new int[9];
+        int max = 0;
+        int index = 0;
+        
+        for (int i = 0; i < 9; i++) {
+            
+            arr[i] = sc.nextInt();
+            
+        }
+       for (int j = 0; j < 9; j++) {
+          
+           if (arr[j] > max) {
+               
+               max = arr[j];
+               index = (j + 1);
+           }
+       }
+    System.out.println(max);
+        System.out.println(index);
+    }
+    
+}
