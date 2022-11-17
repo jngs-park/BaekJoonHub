f0413ffed7db3c2747219f4083465aa9b2a4d82f
jayP <dear.jayp@gmail.com>
2022-11-17 18:59:29 +0900

    [D2] Title: 시각 덧셈, Time: 130 ms, Memory: 27,180 KB -BaekjoonHub

---
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int t=1; t<=T; t++) {
+            int h1 = sc.nextInt();
+            int m1 = sc.nextInt();
+            int h2 = sc.nextInt();
+            int m2 = sc.nextInt();
+            int h = h1 + h2;
+            int m = m1 + m2;
+            if( m > 60 ) {
+                m -= 60;
+                h++;
+            }
+            if( h > 12 ) h -= 12;
+            System.out.printf("#%d %d %d\n", t, h, m);
+        }
+    }
+}
