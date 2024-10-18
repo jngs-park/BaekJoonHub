9b9ead35816a343f0b10c90b61bf877b16094dcc
JP <dear.jayp@gmail.com>
2024-10-18 13:09:57 +0900

    [Bronze II] Title: 벌집, Time: 168 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int count = 1;
+        int range = 2;
+        
+        if ( N == 1) {
+            count = 1;
+        } else {
+            while (range <= N){
+                range += (6 * count);
+                count ++;
+            }
+            
+        }System.out.print(count);
+    }
+}
